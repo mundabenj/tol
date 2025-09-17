31b4aef37323fa8d4e39b5e68e6a8f409c4f9f07
mundabenj <benjamin.mundama@strathmore.edu>
2025-09-17 10:39:29 +0300

    implemented email message templates from a Lang file v2

--- conf.php
+++ conf.php
@@ -26,8 +26,8 @@ $conf['db_name'] = 'tol';
 // Email configuration
 $conf['mail_type'] = 'smtp'; // Options: 'smtp' or 'mail'
 $conf['smtp_host'] = 'smtp.gmail.com';
-$conf['smtp_user'] = 'bbitalex@gmail.com'; // Replace with your actual SMTP email
-$conf['smtp_pass'] = 'wjxy kqig hdry iioh '; // Replace with your actual SMTP password
+$conf['smtp_user'] = 'example@gmail.com'; // Replace with your actual SMTP email
+$conf['smtp_pass'] = 'yourpasswordhere'; // Replace with your actual SMTP password
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
 
